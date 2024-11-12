7e975610ab3f9dc497f43b3a097a39f6cfe80eb7
junyoung <jadw9715@gmail.com>
2024-11-12 20:41:34 +0900

    Feat. Propt

--- js/index.js
+++ js/index.js
@@ -40,8 +40,8 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 //eeeee
 //** Notion 이름짓기 */
-const propMsg = "노션방 이름을 지어주세요.";
-const result = window.prompt(propMsg, "");
-console.log(result);
-const title = document.getElementById("notionTitle");
-title.textContent = `${result || "아무개"}의 Notion`;
+// const propMsg = "노션방 이름을 지어주세요.";
+// const result = window.prompt(propMsg, "");
+// console.log(result);
+// const title = document.getElementById("notionTitle");
+// title.textContent = `${result || "아무개"}의 Notion`;
