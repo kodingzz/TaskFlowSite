f101916aa1767491321b13127c0442860d2b8eab
hyunw00 <khwland090@gmail.com>
2024-11-14 01:19:20 +0900

    Fix. 검색기능 경로버그 수정

--- js/utils.js
+++ js/utils.js
@@ -2,6 +2,20 @@ import { handleGetAllDocs, handleGetDocById } from "./client.js";
 
 let docLists = await handleGetAllDocs();
 
+const EDITOR_TEMP = ` <div class="editor-content">
+<div class="title-container">
+  <input
+    id="title-input"
+    class="title-input"
+    placeholder="제목"
+  ></input>
+</div>
+<div id="text-container">
+<div class="text-block" contenteditable="true"></div>
+</div>
+</div>
+`;
+
 export function loadEditorScript() {
   const id = `editor-script`;
   if (!document.getElementById(id)) {
@@ -21,6 +35,42 @@ export async function loadTextEditor(id) {
   paths.forEach((item) => {
     dirContent += `<span>/</span><a href="/documents/${item.id}" data-url="${item.id}">${item.title}</a>`;
   });
+
+  const content =
+    id === "Content"
+      ? `
+      <div class="editor-top">
+    <div class="editor-dir">${dirContent}</div>
+  </div>
+      <div class="intro">Hello World</div>`
+      : id
+      ? `
+    <div class="editor-top">
+    <div class="editor-dir">${dirContent}</div>
+  </div>
+ ${EDITOR_TEMP}
+  `
+      : "<h1>페이지를 찾을 수 없습니다.</h1>";
+  editor.innerHTML = content;
+
+  // 경로 읽기
+  document.querySelector(".editor-dir").addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const id = e.target.dataset.url;
+
+    if (!id) {
+      history.pushState({ page: "/" }, "", `/`); // root로 이동
+      loadTextEditor("Content");
+    } else {
+      history.pushState({ page: id }, "", `/documents/${id}`);
+      loadTextEditor(id);
+    }
+  });
+  loadEditorScript();
+  const isMenuClose = localStorage.getItem("isMenuClose");
+  console.log("isMenuClose", isMenuClose);
+  if (isMenuClose === "true") makeOpenSidebarBtn();
 }
 
 export async function pathfromRoot(docId, docList) {
