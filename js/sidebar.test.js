562a46868b86fe98491fa9c7f6b45bd4b949d155
junyoung <jadw9715@gmail.com>
2024-11-13 12:17:11 +0900

    Feat. find id

--- js/sidebar.test.js
+++ js/sidebar.test.js
@@ -11,10 +11,12 @@ const sidebarItems = document.querySelector(".sidebar-nav ul");
 const addDocBtn = document.querySelector("#createDocBtn");
 const editor = document.querySelector("#editor");
 
+let docList = [];
+
 async function loadSidebarDocs() {
   sidebarItems.innerHTML = "";
   const documents = await handleGetAllDocs();
-
+  docList = documents;
   documents.forEach((doc) => {
     addDoc(doc);
   });
@@ -72,15 +74,36 @@ async function addDoc(doc) {
   sidebarItems.appendChild(makeItem(doc));
 }
 
+function findDocID(list, id) {
+  for (const doc of list) {
+    if (doc.id === id) {
+      console.log(doc);
+    }
+    if (doc.documents.length > 0) {
+      const result = findDocID(doc.documents, id);
+      if (result) {
+        console.log(result);
+      }
+    }
+  }
+}
+
 // URL에 맞는 콘텐츠 로드 (동적으로 콘텐츠를 로드하는 함수)
 function loadTextEditor(id) {
+  console.log(id);
+  let dirContent = '<a href="/">Home</a>';
+  if (id) findDocID(docList, id);
   const content =
     id === "Content"
-      ? `<div class="intro">어서오세요</div>`
+      ? `
+      <div class="editor-top">
+      <div class="editor-dir">${dirContent}</div>
+    </div>
+      <div class="intro">어서오세요</div>`
       : id
       ? `
     <div class="editor-top">
-    <div class="editor-dir">root1 </div>
+    <div class="editor-dir">${dirContent}</div>
   </div>
   <div class="editor-content">
     <h2 id="title-display"></h2>
