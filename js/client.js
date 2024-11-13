1a7509b015aba555644cad3c84f3a05324757854
hyunw00 <khwland090@gmail.com>
2024-11-14 02:25:37 +0900

    Complete. 에러수정완료

--- js/client.js
+++ js/client.js
@@ -1,5 +1,6 @@
 const BASE_URL = "https://kdt-api.fe.dev-cos.com/documents";
 // const USER_NAME = "CN-4";
+const USER_NAME = "HW-00";
 
 const handleFetch = async (endPoint = "", options) => {
   const fetchOpts = {
