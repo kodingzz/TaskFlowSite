12f4b9c7e32e32861e94e6d835802a314d046494
junyoung <jadw9715@gmail.com>
2024-11-14 16:32:49 +0900

    Fix. editor

--- js/client.js
+++ js/client.js
@@ -1,5 +1,5 @@
 const BASE_URL = "https://kdt-api.fe.dev-cos.com/documents";
-const USER_NAME = "CH-4";
+const USER_NAME = "CH-4-sdf";
 
 const handleFetch = async (endPoint = "", options) => {
   const fetchOpts = {
