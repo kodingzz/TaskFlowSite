951d5919744467970d36a6cf3984ae96fd6d15a8
hyunw00 <khwland090@gmail.com>
2024-11-14 19:18:51 +0900

    Update client.js

--- js/client.js
+++ js/client.js
@@ -1,5 +1,5 @@
 const BASE_URL = "https://kdt-api.fe.dev-cos.com/documents";
-const USER_NAME = "CH-4";
+const USER_NAME = "HW-111";
 
 const handleFetch = async (endPoint = "", options) => {
   const fetchOpts = {
